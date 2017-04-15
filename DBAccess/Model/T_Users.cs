05e22962971e0371aa859168427b7ce0597b95df
haozhiying <hzy_12@live.com>
2017-04-15 10:26:42 +0800

    修复edit，add函数对null的处理。

--- DBAccess/Model/T_Users.cs
+++ DBAccess/Model/T_Users.cs
@@ -33,6 +33,40 @@ namespace DBAccess.Model
         //[Filed(DisplayName = "ID", IsPrimaryKey = true)]
         //public Guid? uUsers_ID
         //{
+        //    set { SetValue("uUsers_ID", value); }
+        //    get { return GetValue<Guid?>("uUsers_ID"); }
+        //}
+        //[Filed(DisplayName = "真实姓名")]
+        //[CRequired(ErrorMessage = "请输入{name}")]
+        //public string cUsers_Name
+        //{
+        //    set { SetValue("cUsers_Name", value); }
+        //    get { return GetValue<string>("cUsers_Name"); }
+        //}
+        //public string cUsers_LoginName
+        //{
+        //    set { SetValue("cUsers_LoginName", value); }
+        //    get { return GetValue<string>("cUsers_LoginName"); }
+        //}
+        //public string cUsers_LoginPwd
+        //{
+        //    set { SetValue("cUsers_LoginPwd", value); }
+        //    get { return GetValue<string>("cUsers_LoginPwd"); }
+        //}
+        //public string cUsers_Email
+        //{
+        //    set { SetValue("cUsers_Email", value); }
+        //    get { return GetValue<string>("cUsers_Email"); }
+        //}
+        //public DateTime? dUsers_CreateTime
+        //{
+        //    set { SetValue("dUsers_CreateTime", value); }
+        //    get { return GetValue<DateTime?>("dUsers_CreateTime"); }
+        //}
+
+        //[Filed(DisplayName = "ID", IsPrimaryKey = true)]
+        //public Guid? uUsers_ID
+        //{
         //    set { SetValue(MethodBase.GetCurrentMethod().Name, value); }
         //    get { return GetValue<Guid?>(MethodBase.GetCurrentMethod().Name); }
         //}
@@ -67,6 +101,7 @@ namespace DBAccess.Model
         //    set { SetValue(MethodBase.GetCurrentMethod().Name, value); }
         //    get { return GetValue<DateTime?>(MethodBase.GetCurrentMethod().Name); }
         //}
+
 
     }
 }
